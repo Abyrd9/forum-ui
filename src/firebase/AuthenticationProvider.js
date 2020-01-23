8a7a973f5dc6d32091bcccf62c6c2bab001cb2fd
Abyrd9 <andrew.byrd99@gmail.com>
2020-01-23 15:21:44 -0800

    changing up firebase initialization

--- src/firebase/AuthenticationProvider.js
+++ src/firebase/AuthenticationProvider.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 import firebase from 'firebase';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCiPHD2ZQMVDpTl3QEe2YoF7TU4pmE0FaQ",
-  authDomain: "forumui-92814.firebaseapp.com",
-  databaseURL: "https://forumui-92814.firebaseio.com",
-  projectId: "forumui-92814",
-  storageBucket: "forumui-92814.appspot.com",
-  messagingSenderId: "626447249465",
-  appId: "1:626447249465:web:c46aab1a8ee8ab04ce5de8",
-  measurementId: "G-KHP7DMFNNK"
+  apiKey: 'AIzaSyCiPHD2ZQMVDpTl3QEe2YoF7TU4pmE0FaQ',
+  authDomain: 'forumui-92814.firebaseapp.com',
+  databaseURL: 'https://forumui-92814.firebaseio.com',
+  projectId: 'forumui-92814',
+  storageBucket: 'forumui-92814.appspot.com',
+  messagingSenderId: '626447249465',
+  appId: '1:626447249465:web:c46aab1a8ee8ab04ce5de8',
+  measurementId: 'G-KHP7DMFNNK',
 });
 
 const auth = firebase.auth();
@@ -18,21 +18,21 @@ export const AuthenticationContext = React.createContext({});
 const AuthenticationProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   useEffect(() => {
-    const unlisten = firebase.auth().onAuthStateChanged(
-      authUser => {
-        if (authUser) {
-          setUser(authUser)
-        } else {
-          setUser(null);
-        }
+    const unlisten = firebase.auth().onAuthStateChanged(authUser => {
+      if (authUser) {
+        setUser(authUser);
+      } else {
+        setUser(null);
       }
-    );
+    });
     return () => {
       unlisten();
-    }
+    };
   }, []);
   return (
-    <AuthenticationContext.Provider value={{ auth, user }}>{children}</AuthenticationContext.Provider>
+    <AuthenticationContext.Provider value={{ auth, user }}>
+      {children}
+    </AuthenticationContext.Provider>
   );
 };
 
