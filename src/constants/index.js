0b32afff4c17a739b6eb8a73ffdcdf32f1e76be0
Abyrd9 <andrew.byrd99@gmail.com>
2020-07-07 16:12:27 -0400

    adding real url to the email authentication

--- src/constants/index.js
+++ src/constants/index.js
@@ -77,6 +77,6 @@ export const ACTION_CODE_SETTINGS = {
     window.location.hostname === "localhost" ||
     window.location.hostname === "127.0.0.1"
       ? "http://localhost:3000"
-      : "",
+      : "http://forumui-92814.firebaseapp.com/",
   handleCodeInApp: true
 };
